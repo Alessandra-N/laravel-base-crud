91be1344dbb79f6c422561fa364f23a1b75d50ae
Alessandra-N <nardella.alessandra1987@gmail.com>
2021-07-15 17:28:45 +0200

    edit reso funzionante

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,22 +4,22 @@
   <div class="container_form">
     <div class="form-group">
 
-      <form action="" method="POST">
-        {{ csrf_field() }}
+      <form action="{{route('update', $comics->id)}}" method="POST">
+        @csrf
         @method('PUT')
-        <label for="{{route('update', $comics->id)}}"></label>
+        <label for="title"></label>
         <input type="text" name="title" id="title" class="form-control" placeholder="" aria-describedby="helpId" value="{{$comics->title}}">
         <small id="helpId" class="text-muted">inserisci un titolo</small>
 
-        <label for=""></label>
+        <label for="description"></label>
         <textarea type="text" name="description" id="description" class="form-control" rows="10" placeholder="" aria-describedby="helpId">{{$comics->description}}</textarea>
         <small id="helpId" class="text-muted"></small>
 
-        <label for=""></label>
+        <label for="thumb"></label>
         <input type="text" name="thumb" id="thumb" class="form-control" placeholder="" aria-describedby="helpId" value="{{$comics->thumb}}">
         <small id="helpId" class="text-muted">inserisci il link</small>
 
-        <label for=""></label>
+        <label for="price"></label>
         <input type="text" name="price" id="price" class="form-control" placeholder="" aria-describedby="helpId" value="{{$comics->price}}">
         <small id="helpId" class="text-muted">inserisci il prezzo</small>
 
